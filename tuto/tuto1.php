efc65df621293fa1487018df72da4d50a2debe4c
dysoncodes <dysoninteractiverblx@gmail.com>
2026-08-30 22:00:29 +0800

    Tutorial 1 - Introduction

---
+++ tuto/tuto1.php
@@ -0,0 +1,23 @@
+// video 1
+// php is a recursive acronym for "PHP: Hypertext Preprocessor"
+
+// php is server-side scripting language designed for web development but also used as a general-purpose programming
+language.
+It is widely used for creating dynamic web pages and applications.
+//(it runs on the server and generates HTML that is sent to the client's browser)
+
+// When create a static HTML website with some CSS and JavaScript,
+all the codes you write will run on the client's browser only.
+
+// But when you create a dynamic website with PHP, the PHP code runs on the server and
+generates HTML that is sent to the client's browser. This allows you to create web
+pages that can change based on user input or other factors.
+
+// php code could compile a dynamic HTML template and then send back the
+generated HTML to the client's browser.
+
+// after getting the html page in the browser and browsing it,
+we could fill in a form and submit the form,
+then it will send another request to the server,
+the server will have some php code to take that data and save it to a database,
+once the data is saved, the server will generate a new HTML page and send it back to the client's browser.
