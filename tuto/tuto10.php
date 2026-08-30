bd224779eb387f5d20dee33f4132f6e31ea4e3bf
dysoncodes <dysoninteractiverblx@gmail.com>
2026-08-30 22:07:37 +0800

    Tutorial 10 - Booleans & Comparisions

---
+++ tuto/tuto10.php
@@ -0,0 +1,44 @@
+video10/
+└── index.php Booleans and Comparisons
+
+<?php
+// comparisons booleans (true or false)
+//echo true; "1"
+//echo false;""
+
+//numbers
+//echo 5 < 10;
+//echo 5 == 10;
+//echo 10 == 10;
+//echo 5 != 10;
+//echo 5 <= 5;
+//echo 5 >= 5;
+
+//strings
+//echo 'aiman' < 'aqeil';
+//echo 'aqeil' > 'hanan';
+//echo 'hanan' > 'izat';
+//echo 'poopypants' == 'poopypants';
+//echo 'Poopypants' == 'Poopypants';
+
+// loose vs strict equal comparison
+//echo 5 == '5';
+//echo 5 === '5';
+
+echo true == "1";
+echo false == "";
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My PHP Tutorial</title>
+</head>
+
+<body>
+    <h1>Booleans and Comparisons</h1>
+</body>
+
+</html>
