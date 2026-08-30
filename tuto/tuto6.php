4305b7aae972afdea26a0f3981ad9887a07264f4
dysoncodes <dysoninteractiverblx@gmail.com>
2026-08-30 22:04:39 +0800

    Tutorial 6 - Numbers

---
+++ tuto/tuto6.php
@@ -0,0 +1,46 @@
+video6/
+└── index.php numbers
+
+<?php
+$radius = 25;
+$pi = 3.14;
+
+//basic - , /, +, -, *
+
+//echo $pi * $radius ** 2; //it will calculate the area of a circle
+
+//order of operations (B I M D A S) - Brackets, Indices, Multiplication, Division, Addition, Subtraction
+
+//echo 2 * (4 + 9) / 3; //it will calculate the order of operations
+
+//increment and decrement operators
+
+//echo $radius--; //it will print the value of radius and then decrement it by 1
+//echo $radius;
+
+//shorthand operators
+
+$age = 20;
+//$age -= 10;
+//$age *= 2;
+///echo $age;
+
+// number functions
+
+//echo floor($pi); //it will round down the value of pi
+//echo ceil($pi); //it will round up the value of pi
+//echo pi()
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My PHP Tutorial</title>
+</head>
+
+<body>
+    <h1>Number Example</h1>
+</body>
+
+</html>
