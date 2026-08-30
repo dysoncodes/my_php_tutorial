b14cf98ec92a4c53e195c01db54cbe8dd298cfe7
dysoncodes <dysoninteractiverblx@gmail.com>
2026-08-30 21:51:28 +0800

    This is the main index of the code.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// PHP code goes here
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<!-- Include the header template -->
+<?php include('templates/header.php'); ?>
+
+<!-- Include the footer template -->
+<?php include('templates/footer.php'); ?>
+
+</html>
